cd9dd22e907bbdb7bcd3879e03ab717598c602cc
Jan benák <j.benak8@gmail.com>
2023-02-15 15:45:03 +0100

    change FXML to show in low resolution 1366x768.

--- src/main/java/cz/jbenak/tezamv/ardonFeedConverter/Main.java
+++ src/main/java/cz/jbenak/tezamv/ardonFeedConverter/Main.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 
 public class Main extends Application {
 
-    public static final String VERSION = "1.0.0.0";
+    public static final String VERSION = "1.0.0.1";
     private static Main instance;
     private Properties appSettings;
     private boolean isProcessing = false;
